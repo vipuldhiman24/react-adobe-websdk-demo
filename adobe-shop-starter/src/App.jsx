b16079fdd208f6f5b025903df3e2ce2832ece8eb
Your New Name <vipuldhiman24@gmail.com>
2026-05-21 19:02:35 +0530

    quick update

--- adobe-shop-starter/src/App.jsx
+++ adobe-shop-starter/src/App.jsx
@@ -1,24 +1,88 @@
 import {
   Routes,
   Route,
+  useLocation,
 } from "react-router-dom";
 
+import {
+  useEffect,
+} from "react";
+
 import Home from "./pages/Home";
-import ProductDetails from "./pages/ProductDetails";
-import CartPage from "./pages/CartPage";
+
+import ProductDetails
+  from "./pages/ProductDetails";
+
+import CartPage
+  from "./pages/CartPage";
 
 export default function App() {
+
+  const location =
+    useLocation();
+
+  useEffect(() => {
+
+    let viewName = "Home";
+
+    if (
+      location.pathname.includes(
+        "/product"
+      )
+    ) {
+
+      viewName =
+        "Product Details";
+    }
+
+    if (
+      location.pathname.includes(
+        "/cart"
+      )
+    ) {
+
+      viewName = "Cart";
+    }
+
+    console.log(
+      "Adobe SPA View:",
+      viewName
+    );
+
+    document.dispatchEvent(
+      new CustomEvent(
+        "adobe-spa-view",
+        {
+          detail: {
+            viewName
+          }
+        }
+      )
+    );
+
+  }, [location.pathname]);
+
   return (
+
     <Routes>
-      <Route path="/" element={<Home />} />
+
+      <Route
+        path="/"
+        element={<Home />}
+      />
+
       <Route
         path="/product/:id"
-        element={<ProductDetails />}
+        element={
+          <ProductDetails />
+        }
       />
+
       <Route
         path="/cart"
         element={<CartPage />}
       />
+
     </Routes>
   );
 }
